9f0655a0d926d146455be21a9aee09cf275644f9
lumix103 <fabiant941@gmail.com>
2020-05-30 19:58:18 -0700

    Fixed Bugs in Mult & Get User Input Subroutine

--- main.asm
+++ main.asm
@@ -1,9 +1,12 @@
 .ORIG	x3000
-    ; TODO: Start
+    AND R0, R0, #0
+    JSR GET_USER_INPUT
     HALT
-
+    PANIC 
+        HALT
     GET_USER_INPUT
-        JSR SAVE_REG            ; Need to save R7
+        ST R7, TEMP
+        JSR SAVE_REG
         AND R1, R1, #0          ; Store result in R1
         GUI_LOOP
             AND R2, R2, #0      ; Temp storage
@@ -24,36 +27,42 @@
             LD R4, MULT_Y       ; Load x3102
             AND R5, R5, #0      ; Clear R5
             ADD R5, R5, xA      ; R5 = 10
-            STR R2, R3, #0      ; Store value of R2 at address hold by R3
+            STR R1, R3, #0      ; Store value of R1 at address hold by R3
             STR R5, R4, #0      ; Store value of R5 at address hold by R4
             JSR SAVE_REG        ; Save registers
             JSR MULT            ; Jump to subroutine MULT
             JSR LOAD_REG        ; Load registers
             LD R3, MULT_R       ; Load x3103
             LDR R3, R3, #0      ; Load value from address hold by R3 to R3
+            ADD R1, R3, R2      ; R1 = R3 + R2 => R1 = 10 * R1 + R2
+            BR GUI_LOOP
         BREAK_GUI_LOOP
         LD R2, PARSED_RESULT    ; Store result at x3100
         STR R1, R2, #0          ; ---------------------
         JSR LOAD_REG            ; Load registers
+        LD R7, TEMP
     RET
-    MULT                        ; Positive integers only
-        LD R0, MULT_X           ; Load x3101
-        LD R1, MULT_Y           ; Load x3102
-        LDR R0, R0, #0          ; Load value from address hold by R0 to R0
-        LDR R1, R1, #0          ; Load value from address hold by R1 to R1
-        AND R2, R2, #0          ; Clear R2 and use as results
+    MULT
+        LD R0, MULT_X
+        LDR R0, R0, #0
+        LD R1, MULT_Y   ; Counter
+        LDR R1, R1, #0
+        AND R2, R2, #0  ; Result 
         MULT_LOOP
-            ADD R1, R1, #0      ; If R1 == 0 then break
-            BRz BREAK_MULT_LOOP ; ---------------------
-            ADD R2, R2, R1      ; R2 = R2 + R1
-            ADD R1, R1, #-1     ; R1--
-        BREAK_MULT_LOOP
-        LD R0, MULT_R           ; Load value x3103
-        STR R2, R0, #0          ; Store the value of R2 into the address hold by R0
+            ADD R1, R1, #0
+            BRnz MULT_BREAK
+
+            ADD R2, R2, R0
+
+            ADD R1, R1, #-1
+            BR MULT_LOOP
+        MULT_BREAK
+        LD R0, MULT_R
+        STR R2, R0, #0
     RET
     SAVE_REG
-        ST R7, TEMP_SAVE
-        LD R7, STACK_TOP
+        ST R7, REG_TEMP
+        LD R7, REG_STACK
         STR R0, R7, #0
         STR R1, R7, #1
         STR R2, R7, #2
@@ -62,15 +71,15 @@
         STR R5, R7, #5
         STR R6, R7, #6
         ADD R7, R7, #7
-        LD R0, TEMP_SAVE
+        LD R0, REG_TEMP
         STR R0, R7, #0
         ADD R7, R7, #1
-        ST R7, STACK_TOP
-        LD R7, TEMP_SAVE
+        ST R7, REG_STACK
+        LD R7, REG_TEMP
     RET
     LOAD_REG
-        ST R7, TEMP_SAVE        ; Store the current value of R7 as a temp
-        LD R7, STACK_TOP        ; Load the value at the top of the stack
+        ST R7, REG_TEMP        ; Store the current value of R7 as a temp
+        LD R7, REG_STACK        ; Load the value at the top of the stack
         LD R0, EMPTY            ; Load the empty sentinel value 
         NOT R0, R0              ; Two's Complement
         ADD R0, R0, #1          ; ----------------
@@ -90,11 +99,10 @@
 
         LDR R1, R7, #-7         ; Load the value from address hold by R7 with an offset of -7 at R7
         ADD R7, R7, #-8 
-        ST R7, STACK_TOP
+        ST R7, REG_STACK
         LR_BREAK  
-        LD R7, TEMP_SAVE
+        LD R7, REG_TEMP
     RET
-.END
 
 PARSED_RESULT   .FILL	x3100
 
@@ -108,5 +116,7 @@ REG_TEMP        .FILL	x310C
 REG_STACK       .FILL	x310D
 
 EMPTY           .FILL	xC000
-
+TEMP            .FILL	x0
 PROMPT          .STRINGZ	"Enter a number within 0 and 100: "
+
+.END
